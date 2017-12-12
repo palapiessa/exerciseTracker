68ec8243f4c876373312d06a246ab1968385f51d
* <palapiessa@gmail.com>
2017-12-12 08:08:41 +0200

    test stucture added

---
+++ src/activityEvent.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from numbers import Number
+
+class activityEvent: 
+    units=""
+
+    def __init__(self): 
+        self.date = datetime.now()
+        self.units = ['km','mi','h']
+    
+    @property
+    def date(self):                                      return self.__date
+
+    @property 
+    def distance(self): 
+        return self.__distance 
+    
+    @distance.setter 
+    def distance(self, distance): 
+        if not isinstance(distance, Number):
+            raise ValueError("Value is not correct, give positive number")
+        if distance < 0: 
+            raise ValueError("Value is not correct, give positive number")
+
+    @property
+    def unit(self):
+        return self.__unit
+
+    @unit.setter
+    def unit(self, unit):
+        if unit in units:                                    self.__unit = unit
+        else:
+            raise ValueError("Unit is not correct, use km, mi or h.")
+    
--- tests/activityControllerTests.py
+++ tests/activityControllerTests.py
@@ -1,11 +1,17 @@
 import unittest 
+import datetime
+from .context import activityEvent
 
 class activityControllerTest(unittest.TestCase):
     def testConstructorShouldStoreActivity(self):         
-        activity = Activity('20170101',1,1,24.5)
+        activity = activityEvent()
+        activity.distance = 15.5
+        activity.unit = "km"
+
         self.assertEquals(activity.type.description, 'cycling'," activity type not set correctly") 
         self.assertEquals(activity.unit,'km', "unit not set correctly")
-        self.assertEquals(activity.distance,24.5,"distance not set cotrectly")
-        self.assertEquals(activity.date,
+        self.assertEquals(activity.distance,15.5,"distance not set cotrectly")
+        self.assertEquals(activity.date,datetime.date())
+
     if __name__ == '__main__':     
         unittest.main() 
