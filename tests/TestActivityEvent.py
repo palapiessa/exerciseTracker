c2132c40e2a002cb90b3ab0af67b91b5623f0dd6
* <palapiessa@gmail.com>
2018-02-09 08:32:58 +0200

    Timer functions separated from ActivityEvent class. Class names follow convention.

--- tests/TestActivityEvent.py
+++ tests/TestActivityEvent.py
@@ -1,22 +1,22 @@
 import unittest 
 from datetime import datetime
-from context import activityEvent
+from context import ActivityEvent
 
 class TestActivityEvent(unittest.TestCase):
     
     def test_ConstructorShouldStoreActivity(self):
         testStart = self.getCurrentTime()
-        activity = activityEvent
+        activity = ActivityEvent
         activity.distance = 15.5
         activity.unit = "km"
         activity.type = "cycling"
         self.assertEqual(activity.type, "cycling"," activity type not set correctly")
         self.assertEqual(activity.unit,'km', "unit not set correctly")
         self.assertEqual(activity.distance,15.5,"distance not set cotrectly")
-        self.assertEqual(activity().startTime,testStart, "activity start date:" +activity().startTime+" testStart:"+testStart)
-        activity.stop()
-        activityEnd = self.getCurrentTime()
-        self.assertEqual(activity().endTime,activityEnd, "activity end date:" +activity().endTime+" test end:"+activityEnd)
+        #self.assertEqual(activity().startTime,testStart, "activity start date:" +activity().startTime+" testStart:"+testStart)
+        #activity.stop()
+        #activityEnd = self.getCurrentTime()
+        #self.assertEqual(activity().endTime,activityEnd, "activity end date:" +activity().endTime+" test end:"+activityEnd)
 
     def getCurrentTime(self):
         currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
--- tests/TestTimer.py
+++ tests/TestTimer.py
@@ -1,6 +1,6 @@
 import unittest
 from datetime import datetime
-from context import activityEvent
+from context import Timer
 
 class TestTimer(unittest.TestCase):
 
@@ -10,11 +10,24 @@ class TestTimer(unittest.TestCase):
         self.assertEqual(timer().startTime,testStart, "timer start date:" +timer().startTime+" testStart:"+testStart)
 
     def test_Stop(self):
-        timer = Timer
+        timer =Timer
         testStop = self.getCurrentTime() 
         timer.stop()
+        self.assertEqual(timer().stopTime,testStop, "timer stop date:" +timer().stopTime+" testStop:"+testStop)
 
+    def test_Duration(self):
+        duration = self.getDuration(testStart,testStop)
+        self.assertEqual(timer().duration,duration, "timer duration:" +timer().duration+" testDuration:"+duration)
+        
+    def getCurrentTime(self):
+        currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return currentTime
+    
+    def getDuration(self, testStart, testEnd):
+        start= datetime.strptime(testStart,"%Y-%m-%d %H:%M:%S")
+        end= datetime.strptime(testEnd,"%Y-%m-%d %H:%M:%S")
+        elapsed = end - start
+        return divmod(elapsed.days * 86400 + elapsed.seconds, 60)
 
-#  def test_Duration(self):
-
+       
 if __name__ == '__main__':                           unittest.main()
