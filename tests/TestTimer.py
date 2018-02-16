91ab7f0f4073467346d3e0dff4c3b492e52c9099
* <palapiessa@gmail.com>
2018-02-16 16:37:03 +0200

    Timer class ready

--- tests/TestTimer.py
+++ tests/TestTimer.py
@@ -4,20 +4,22 @@ from context import Timer
 import time 
 
 class TestTimer(unittest.TestCase):
-    timer = None
 
     def test_Start(self):
+        timer = Timer()
         startTime = self.getCurrentTime()
         self.assertEqual(timer.startTime,startTime, "timer start date:" +timer.startTime+" testStart:"+startTime)
 
     def test_Stop(self):
+        timer = Timer()
+        time.sleep(2.0)
         stopTime = self.getCurrentTime() 
         timer.stop()
         self.assertEqual(timer.stopTime, stopTime , "timer stop date:" +timer.stopTime+" testStop:"+stopTime) 
 
     def test_Duration(self):
         timer2 = Timer()
-        time.sleep = 3.0
+        time.sleep(3.0)
         timer2.stop()
         duration= (0,3)
         self.assertEqual(timer2.duration()[0],duration[0])
@@ -28,6 +30,4 @@ class TestTimer(unittest.TestCase):
         return currentTime
     
 if __name__ == '__main__':
-    global timer
-    timer = Timer() 
     unittest.main()
