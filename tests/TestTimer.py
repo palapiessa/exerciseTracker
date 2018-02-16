a625b05e1963aaf0b4bb36e31e746ab562d68cce
* <palapiessa@gmail.com>
2018-02-16 12:34:07 +0200

    Timer class tests added

--- tests/TestTimer.py
+++ tests/TestTimer.py
@@ -1,33 +1,33 @@
 import unittest
 from datetime import datetime
 from context import Timer
+import time 
 
 class TestTimer(unittest.TestCase):
+    timer = None
 
     def test_Start(self):
-        testStart = self.getCurrentTime()
-        timer = Timer
-        self.assertEqual(timer().startTime,testStart, "timer start date:" +timer().startTime+" testStart:"+testStart)
+        startTime = self.getCurrentTime()
+        self.assertEqual(timer.startTime,startTime, "timer start date:" +timer.startTime+" testStart:"+startTime)
 
     def test_Stop(self):
-        timer =Timer
-        testStop = self.getCurrentTime() 
+        stopTime = self.getCurrentTime() 
         timer.stop()
-        self.assertEqual(timer().stopTime,testStop, "timer stop date:" +timer().stopTime+" testStop:"+testStop)
+        self.assertEqual(timer.stopTime, stopTime , "timer stop date:" +timer.stopTime+" testStop:"+stopTime) 
 
     def test_Duration(self):
-        duration = self.getDuration(testStart,testStop)
-        self.assertEqual(timer().duration,duration, "timer duration:" +timer().duration+" testDuration:"+duration)
-        
+        timer2 = Timer()
+        time.sleep = 3.0
+        timer2.stop()
+        duration= (0,3)
+        self.assertEqual(timer2.duration()[0],duration[0])
+        self.assertEqual(timer2.duration()[1],duration[1])
+
     def getCurrentTime(self):
         currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         return currentTime
     
-    def getDuration(self, testStart, testEnd):
-        start= datetime.strptime(testStart,"%Y-%m-%d %H:%M:%S")
-        end= datetime.strptime(testEnd,"%Y-%m-%d %H:%M:%S")
-        elapsed = end - start
-        return divmod(elapsed.days * 86400 + elapsed.seconds, 60)
-
-       
-if __name__ == '__main__':                           unittest.main()
+if __name__ == '__main__':
+    global timer
+    timer = Timer() 
+    unittest.main()
