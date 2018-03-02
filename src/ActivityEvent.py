45e8a89de5ce8cbb6359a4abf17b1fac1ce557f9
* <palapiessa@gmail.com>
2018-03-02 11:02:55 +0200

    ActivityEvent contructor changed

---
+++ src/ActivityEvent.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from numbers import Number
+
+class ActivityEvent(): 
+    units=""
+    types=""
+
+    def __init__(self, types, units): 
+        self.units = units
+        self.types = types
+
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        if value in self.types:
+            self.__type = value
+        else:
+            raise ValueError("Type is not correct, use " +str(types))
+    
+    @property 
+    def distance(self): 
+        return self.__distance 
+    
+    @distance.setter 
+    def distance(self, value): 
+        if not isinstance(value, Number):
+            raise ValueError("Value is not correct, give positive number")
+        elif value < 0: 
+            raise ValueError("Value is not correct, give positive number")
+        else:
+            self.__distance = value
+
+    @property
+    def unit(self):
+        return self.__unit
+
+    @unit.setter
+    def unit(self, unit):
+        if unit in self.units:
+            self.__unit = unit
+        else:
+            raise ValueError("Unit is not correct, use "+str(units))
+    
---
+++ src/Timer.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from numbers import Number
+
+class Timer():
+    
+    def __init__(self):
+        self.startTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+     
+    def stop(self):
+        self.stopTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+           
+    @property
+    def startTime(self):
+        return self.__startTime
+    
+    @startTime.setter
+    def startTime(self, date):
+        self.__startTime = date
+
+    @property
+    def stopTime(self):
+        return self.__stopTime
+    
+    @stopTime.setter
+    def stopTime(self, date):
+        self.__stopTime = date
+
+    def duration(self):
+        start= datetime.strptime(self.startTime,"%Y-%m-%d %H:%M:%S")
+        end= datetime.strptime(self.stopTime,"%Y-%m-%d %H:%M:%S")
+        elapsed = end - start
+        return divmod(elapsed.days * 86400 + elapsed.seconds, 60)
