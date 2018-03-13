5e89bcdab4fad290c21d851f2895d7f81f5adb48
* <palapiessa@gmail.com>
2018-03-13 09:45:21 +0200

    argument parser test added

--- tests/TestActivityEvent.py
+++ tests/TestActivityEvent.py
@@ -8,13 +8,13 @@ class TestActivityEvent(unittest.TestCase):
         activity = self.createActivityEvent()
         activity.distance = 15.5
         activity.unit = "km"
-        activity.type = "cycling"
-        self.assertEqual(activity.type, "cycling"," activity type not set correctly")
+        activity.type = "cycle"
+        self.assertEqual(activity.type, "cycle"," activity type not set correctly")
         self.assertEqual(activity.unit,'km', "unit not set correctly")
         self.assertEqual(activity.distance,15.5,"distance not set cotrectly")
 
     def createActivityEvent(self):
-        types = ["cycling","sleeping","jogging"]
+        types = ["cycle","sleep","run"]
         units = ["km","min"]
         activity = ActivityEvent(types,units)
         return activity
